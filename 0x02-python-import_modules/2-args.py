599b5c6daa210621fd9ed2667fe7b58e727c336d
Perockzii <perockzii@gmail.com>
2023-12-05 08:47:08 -0800

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,8 +4,8 @@ if __name__ == "__main__":
     x = len(argv) - 1
     if x < 1:
         print("{} arguments.".format(x))
-    elif x == 1:
-        print("{} argument:".format(x))
+     elif x == 1:
+         print("{} argument:".format(x))
     else: 
         print("{} arguments:".format(x))
     for i in range(x):
